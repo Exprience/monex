e288151231c99ba159ca879655b451faaec4a3b9
uuganbat <uuganbat@innosol.mn>
2016-06-24 10:39:48 +0900

    uuganaa

--- monex/settings.py
+++ monex/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = '/var/www/monex.com/static' #os.path.join(BASE_DIR, "..", "www", "static")
 
 STATICFILES_DIRS = (
     ( 'assets', os.path.join(BASE_DIR, 'static') ),
