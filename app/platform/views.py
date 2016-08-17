1dd3e123f799975e5c63937b06119cd45630a3d3
Nomundari <aki2kioku@gmail.com>
2016-08-17 11:13:06 +0900

    n

--- app/platform/views.py
+++ app/platform/views.py
@@ -5,9 +5,7 @@
 from django.views import generic as g
 
 
-
-
 class Home(g.TemplateView):
-
-
 	template_name = 'competition/home.html'
+class lol (g.TemplateView):
+	template_name = 'competition/popup.html'
